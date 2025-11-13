486d635d5cc37aad21c7ab6cf1eaeeb5744a6890
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-13 10:09:22 +0300

    Work in progress

--- src/MarginCallPluginInterface.cpp
+++ src/MarginCallPluginInterface.cpp
@@ -18,80 +18,22 @@ extern "C" void CreateReport(rapidjson::Value& request,
                              rapidjson::Value& response,
                              rapidjson::Document::AllocatorType& allocator,
                              CServerInterface* server) {
-    std::cout << "Creating margin call report" << std::endl;
+    LogJSON("request: ", request);
 
+    std::vector<GroupRecord> groups;
     std::vector<AccountRecord> accounts;
-    const std::string group = "demo";
-    int result = 2;
 
-    LogJSON("request", request);
-
-    // try {
-    //     result = server->GetAccountsByGroup(group, &accounts);
-    // } catch (const std::exception& e) {
-    //     std::cerr << e.what() << std::endl;
+    const int groups_result = server->GetAllGroups(&groups);
+    // if (group_result != 0) {
+    //
     // }
 
-    result = server->GetAccountsByGroup(group, &accounts);
+     for (const GroupRecord& group : groups) {
+         server->GetAccountsByGroup(group.group, &accounts);
+     }
 
-    std::cout << "Result: " <<  result << std::endl;
-    std::cout << "Accounts size: " <<  accounts.size() << std::endl;
-
-
-    struct DayData {
-        std::string day;
-        double commission;
-        double profit;
-    };
-
-    std::vector<DayData> data = {
-        {"Mon", 120.5, 80.2},
-        {"Tue", 150.3, 95.6},
-        {"Wed", 130.1, 88.0},
-        {"Thu", 170.2, 110.3},
-        {"Fri", 160.8, 102.4}
-    };
-
-    // ---------- Таблица ----------
-    auto makeTable = [&](const std::vector<DayData>& rows) -> Node {
-        std::vector<Node> tableRows;
-
-        // Заголовок
-        tableRows.push_back(tr({
-            th({ text("Login") }),
-            th({ text("Name") }),
-            th({ text("Leverage") }),
-            th({ text("Balance") }),
-            th({ text("Credit") }),
-            th({ text("Floating P/L") }),
-            th({ text("Equity") }),
-            th({ text("Margin") }),
-            th({ text("Free Margin") }),
-            th({ text("Margin Limits") }),
-            th({ text("Margin Level") }),
-            th({ text("Add. Margin") }),
-            th({ text("Currency") }),
-        }));
-
-        // Динамическое формирование строк (через лямбду)
-        for (const auto& row : rows) {
-            tableRows.push_back(tr({
-                td({ text(row.day) }),
-                td({ text(std::to_string(row.commission)) }),
-                td({ text(std::to_string(row.profit)) })
-            }));
-        }
-
-        return table(tableRows, props({{"className", "data-table"}}));
-    };
-
-    Node report = div({
-        h1({ text("Margin Call Report") }),
-        makeTable(data)
-    }, props({{"className", "report"}}));
-
-
-    to_json(report, response, allocator);
+    std::cout << "Groups size: " << groups.size()<< std::endl;
+    std::cout << "Accounts size: " << accounts.size()<< std::endl;
 }
 
 void LogJSON(const std::string& name, const Value& value) {
